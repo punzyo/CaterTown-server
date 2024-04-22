f6d71b13ef1822434e6bdc938467725c852d59e2
yili <hellomickey004@gmail.com>
2024-04-22 16:11:54 +0800

    feat: test Github webhook

--- server.js
+++ server.js
@@ -2,24 +2,32 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { AccessToken } from 'livekit-server-sdk';
+import admin from 'firebase-admin';
 
 const app = express();
 app.use(cors());
+app.use(express.json()); 
+
 const port = 3000;
 
+
+const firebasePrivateKey = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+admin.initializeApp({
+  credential: admin.credential.cert(firebasePrivateKey)
+});
+const db = admin.firestore();
+
+
 const createToken = async (roomId, charName) => {
-  console.log(roomId);
   const at = new AccessToken(
     process.env.LIVEKIT_API_KEY,
     process.env.LIVEKIT_API_SECRET,
     {
       identity: charName,
-      // token to expire after 10 minutes
       ttl: '10m',
     }
   );
   at.addGrant({ roomJoin: true, room: roomId });
-
   return at.toJwt();
 };
 
@@ -36,6 +44,33 @@ app.get('/getToken', async (req, res) => {
   } catch (error) {
     console.error('Failed to create token:', error);
     res.status(500).send('Failed to create token');
+  }
+});
+
+
+app.post('/webhook', async (req, res) => {
+  const payload = req.body;
+
+
+  if (payload.action === 'opened') {
+    const prData = {
+      id: payload.pull_request.id,
+      title: payload.pull_request.title,
+      createdAt: payload.pull_request.created_at,
+      user: payload.pull_request.user.login,
+      state: payload.pull_request.state,
+      url: payload.pull_request.html_url
+    };
+
+    try {
+      await db.collection('pull_requests').doc(prData.id.toString()).set(prData);
+      res.status(200).send('PR data saved to Firebase');
+    } catch (error) {
+      console.error('Firebase error:', error);
+      res.status(500).send('Failed to save PR data');
+    }
+  } else {
+    res.status(200).send('Not a PR opened event');
   }
 });
 
