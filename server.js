692b6aafc546f03fdecb7b0530a7314a471acfbd
punzyo <147231492+punzyo@users.noreply.github.com>
2024-04-24 00:58:09 +0800

    test replace

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.post('/webhook/:roomId', async (req, res) => {
       user: userLogin,
       state: pull_request.state,
       url: pull_request.html_url,
-      description: pull_request.body.replaceAll("\\r?\\n", "\r\n") || '',
+      description: pull_request.body.replace(/\r\n/g, "\\n") || '',
     };
 
     const docRef = db
